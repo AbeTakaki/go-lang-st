98cbfe441116e2a03dc4d5295542176395dc2909
AbeTakaki <abetakaki0101@gmail.com>
2025-12-21 17:50:50 +0900

    add: ハンドラの定義を増やす

--- myapi/main.go
+++ myapi/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -22,8 +23,36 @@ func main() {
 		io.WriteString(w, "Hello, World!\n")
 	}
 
+	// Posting Article ハンドラの定義
+	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Article…\n")
+	}
+	// article listハンドラの定義
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List…\n")
+	}
+	// article No.1 ハンドラの定義
+	article1Handler := func(w http.ResponseWriter, req *http.Request) {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	}
+	// article nice
+	articleNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice…\n")
+	}
+	// comment
+	commentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment…\n")
+	}
+
 	// 定義したハンドラを、サーバーで使用するように登録
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", article1Handler)
+	http.HandleFunc("/article/nice", articleNiceHandler)
+	http.HandleFunc("/comment", commentHandler)
 
 	// サーバー起動時のログを出力
 	log.Println("server start at port 8080")
