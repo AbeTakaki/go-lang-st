c1ef44614906cf386b9a9d2b86f91457afd3c66b
AbeTakaki <abetakaki0101@gmail.com>
2025-12-27 21:44:25 +0900

    feat: リクエストを受け取りそのまま返すように実装

--- myapi/handlers/handlers.go
+++ myapi/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -27,7 +28,28 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 
 // Posting Article ハンドラの定義
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	article := models.Article1
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "cannot get content lengh\n", http.StatusBadRequest)
+		return
+	}
+	reqBodybuffer := make([]byte, length)
+
+	// Read メソッドでリクエストボディを読み出し
+	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
+		return
+	}
+	// ボディをクローズする
+	defer req.Body.Close()
+
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	article := reqArticle
 	jsonData, err := json.MarshalIndent(article, "", "  ")
 	if err != nil {
 		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
