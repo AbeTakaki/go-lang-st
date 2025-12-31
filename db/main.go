9eff1c605e5200249115f2bed66765c4c51e5e41
AbeTakaki <abetakaki0101@gmail.com>
2025-12-31 22:42:55 +0900

    feat: whereを使ったselect文の実装 p.159

--- db/main.go
+++ db/main.go
@@ -28,34 +28,65 @@ func main() {
 	// }
 
 	// select 文の実装
+	// const sqlStr = `
+	// 	select *
+	// 	from articles;
+	// `
+
+	// rows, err := db.Query(sqlStr)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer rows.Close()
+
+	// articleArray := make([]models.Article, 0)
+	// for rows.Next() {
+	// 	var article models.Article
+	// 	var createdTime sql.NullTime
+
+	// 	err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+
+	// 	if createdTime.Valid {
+	// 		article.CreatedAt = createdTime.Time
+	// 	}
+
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	} else {
+	// 		articleArray = append(articleArray, article)
+	// 	}
+	// }
+	// fmt.Printf("%+v\n", articleArray)
+
+	// クエリ定義
+	articleID := 10
 	const sqlStr = `
 		select *
-		from articles;
+		from articles
+		where article_id = ?;
 	`
 
-	rows, err := db.Query(sqlStr)
+	// クエリの実行
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// データベースから取得したデータ変数 article に読み出す
+	var article models.Article
+	var createdTime sql.NullTime
+
+	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer rows.Close()
 
-	articleArray := make([]models.Article, 0)
-	for rows.Next() {
-		var article models.Article
-		var createdTime sql.NullTime
-
-		err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
-
-		if createdTime.Valid {
-			article.CreatedAt = createdTime.Time
-		}
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			articleArray = append(articleArray, article)
-		}
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
 	}
-	fmt.Printf("%+v\n", articleArray)
+
+	fmt.Printf("%+v\n", article)
 }
